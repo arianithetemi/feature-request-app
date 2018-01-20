888d3c583de691185be799d6e73ae4114dea2a14
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-20 17:00:27 +0100

    activating user endpoint added

--- app/mod_user_api/views.py
+++ app/mod_user_api/views.py
@@ -58,12 +58,14 @@ def add_user():
 
 @mod_user_api.route('/<public_id>', methods=['GET'])
 def get_user(public_id):
-
+    # query user in db by public_id
     user = User.query.filter_by(public_id=public_id).first()
 
+    # if user not found
     if not user:
         return jsonify({'message': 'No user found!'})
 
+    # building JSON object for found user
     user_data = {}
     user_data['public_id'] = user.public_id
     user_data['first_name'] = user.first_name
@@ -73,4 +75,20 @@ def get_user(public_id):
     user_data['role'] = user.role.name
     user_data['active'] = user.active
 
+    # returning user_data in json
     return jsonify({'user': user_data})
+
+@mod_user_api.route('/activate/<public_id>', methods=['PUT'])
+def activate_user(public_id):
+    # query user in db by public_id
+    user = User.query.filter_by(public_id=public_id).first()
+
+    # if user not found
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    # Activating user in db
+    user.active = True
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been activated!'})
