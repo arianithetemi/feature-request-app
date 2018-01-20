1c76e203fafc9ca593f4aea20cd413ef5f35ea1e
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-20 01:51:09 +0100

    Get all users completed

--- app/mod_api/views.py
+++ app/mod_api/views.py
@@ -12,6 +12,23 @@ def index():
     '''
     return "Welcome to the API."
 
+@mod_api.route('/user', methods=['GET'])
+def get_all_users():
+
+    users = User.query.all()
+
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['first_name'] = user.first_name
+        user_data['last_name'] = user.last_name
+        user_data['username'] = user.username
+        user_data['email_address'] = user.email_address
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
 @mod_api.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
