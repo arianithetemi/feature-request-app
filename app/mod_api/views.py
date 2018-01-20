a0f85ca9133579cf49e3680b972fb61949e8774f
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-20 01:48:49 +0100

    create new user added in api

--- app/mod_api/views.py
+++ app/mod_api/views.py
@@ -11,3 +11,16 @@ def index():
     :return:
     '''
     return "Welcome to the API."
+
+@mod_api.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+
+    hash_password = bcrypt.generate_password_hash(data['password'])
+
+    new_user = User(public_id=str(uuid.uuid4()), first_name=data['first_name'], last_name=data['last_name'], username=data['username'], email_address=data['email_address'], password=hash_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
