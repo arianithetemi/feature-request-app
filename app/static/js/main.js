e9b5d4e1a799072e6c7903df504fb69c6d9b0be0
Arianit Hetemi <nitihetemi@gmail.com>
2018-01-22 22:48:50 +0100

    Bootstrap 4 implemented, frontend improvements

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
    $('#sign-up-btn').click(function() {
       $('#sign-in').hide();
       $('#sign-up').show()
-      $('.sign-up').css({'margin-top': '2%'});
+      $('.sign-up').css({'margin-top': '6%'});
    });
    $('#sign-in-btn').click(function() {
       $('#sign-in').show();
       $('#sign-up').hide();
-      $('.sign-up').css({'margin-top': '15%'});
+      $('.sign-up').css({'margin-top': '17%'});
    });
 });
