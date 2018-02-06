10455daa6fdb64fa60e978f2649d4126ae0bfd82
Arianit Hetemi <nitihetemi@gmail.com>
2018-02-06 18:34:16 +0100

    Vertically middle aligned fix on index page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
    $('#sign-up-btn').click(function() {
       $('#sign-in').hide();
       $('#sign-up').show()
-      $('.sign-up').css({'margin-top': '6%'});
    });
    $('#sign-in-btn').click(function() {
       $('#sign-in').show();
       $('#sign-up').hide();
-      $('.sign-up').css({'margin-top': '17%'});
    });
 
    var SignInViewModel = {
